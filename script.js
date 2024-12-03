6207ca5b434d289fc9076e188d66a1db1163fecb
Yotin1 <58013144+Yotin1@users.noreply.github.com>
2024-12-03 11:42:58 +0000

    changed animated background

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-var number_of_star = 150;
+const main = document.getElementsByTagName("main")[0];
+const header = document.getElementsByTagName("header")[0];
 
-var random_number = function (min, max) {
+let number_of_star = Math.round((150 * (main.offsetHeight + screen.width)) / 2000);
+
+let random_number = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var createStars = function () {
-  var star_rotation = "move_right;";
-  for (var i = 0; i < number_of_star; i++) {
+let createStars = function () {
+  let star_rotation = "move_right;";
+  for (let i = 0; i < number_of_star; i++) {
     rot = star_rotation == "move_right;" ? "move_left;" : "move_right;";
-    var star_top = random_number(0, document.documentElement.clientHeight);
-    var star_left = random_number(0, document.documentElement.clientWidth);
-    var star_radius = random_number(0, 4);
-    var star_duration = random_number(6, 16);
+    let star_top = random_number(0, main.offsetHeight) + header.offsetHeight;
+    let star_left = random_number(0, screen.width);
+    let star_radius = random_number(0, 4);
+    let star_duration = random_number(6, 16);
 
-    document.body.getElementsByTagName("main")[0].innerHTML +=
+    main.innerHTML +=
       "<div class='star' style='top: " +
       star_top +
       "px; left: " +
