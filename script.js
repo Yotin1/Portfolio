c27bc51af48f1e55f881e12d00ac842d5b1ff433
Yotin1 <58013144+Yotin1@users.noreply.github.com>
2024-12-03 15:55:28 +0000

    changed animated background and page layout

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 const main = document.getElementsByTagName("main")[0];
 const header = document.getElementsByTagName("header")[0];
+const footer = document.getElementsByTagName("footer")[0];
 
-let number_of_star = Math.round((150 * (main.offsetHeight + screen.width)) / 2000);
+let number_of_star = Math.round((150 * (main.offsetHeight + screen.width)) / 1500);
 
 let random_number = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+let stars = [];
 let createStars = function () {
-  let star_rotation = "move_right;";
+  let star_rotation = "move_right";
   for (let i = 0; i < number_of_star; i++) {
-    rot = star_rotation == "move_right;" ? "move_left;" : "move_right;";
-    let star_top = random_number(0, main.offsetHeight) + header.offsetHeight;
-    let star_left = random_number(0, screen.width);
+    rot = star_rotation == "move_right" ? "move_left" : "move_right";
     let star_radius = random_number(0, 4);
-    let star_duration = random_number(6, 16);
+    const star = document.createElement("div");
+    star.className = "star";
+    star.style.top = random_number(header.offsetHeight, screen.height).toString() + "px";
+    star.style.left = random_number(0, screen.width).toString() + "px";
+    star.style.height = star_radius.toString() + "px";
+    star.style.width = star_radius.toString() + "px";
+    star.style.animation = star_rotation + " " + random_number(6, 16).toString() + "s linear 0s infinite";
+    main.append(star);
+    stars.push(star);
+  }
+};
 
-    main.innerHTML +=
-      "<div class='star' style='top: " +
-      star_top +
-      "px; left: " +
-      star_left +
-      "px; width: " +
-      star_radius +
-      "px; height: " +
-      star_radius +
-      "px; " +
-      "animation-name:" +
-      star_rotation +
-      "; animation-duration: " +
-      star_duration +
-      "s;'></div>";
+let updateStars = function () {
+  for (let star of stars) {
+    if (
+      parseInt(star.style.top) < window.innerHeight - footer.offsetHeight + 16 &&
+      parseInt(star.style.left) < window.innerWidth + 12
+    ) {
+      star.style.display = "initial";
+    } else {
+      star.style.display = "none";
+    }
   }
 };
 
 createStars();
+updateStars();
+
+window.addEventListener("resize", updateStars);
